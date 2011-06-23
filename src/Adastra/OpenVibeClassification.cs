5ab855be1040b4eda1bc4624e06ed2d0828957b6
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-06-23 20:18:23 +0000

    Shaping out future release.

---
+++ src/Adastra/OpenVibeClassification.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Adastra
+{
+    /// <summary>
+    /// This form uses the classification result from OpenVibe streamed through OpenVibe.
+    /// The main job is done by OpenVibe, Adastra is used to process (display) the result.
+    /// </summary>
+    public partial class OpenVibeClassification : Form
+    {
+        public OpenVibeClassification()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+        }
+    }
+}
