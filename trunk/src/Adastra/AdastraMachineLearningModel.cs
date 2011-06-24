7d6f2e302ec3602d4448cadd1c98f82bb2939c52
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-06-24 07:08:01 +0000

    Some algorithm functionality encapsulated.

--- trunk/src/Adastra/AdastraMachineLearningModel.cs
+++ trunk/src/Adastra/AdastraMachineLearningModel.cs
@@ -21,12 +21,94 @@ namespace Adastra
             set;
         }
 
-        public AdastraMachineLearningModel(LinearDiscriminantAnalysis lda, ActivationNetwork network)
+        public AdastraMachineLearningModel()
         {
-            _lda = lda;
-            _network = network;
+            //_lda = lda;
+            //_network = network;
 
             ActionList = new Dictionary<string,int>();
+        }
+
+        public void Train(List<double[]> vrpnIncomingSignal, int vrpnDimensions)
+        {
+            #region prepare data
+            // randomize vectors positions
+            // split in training and validation sets 
+            // train NN until validation set
+            #endregion
+
+            double[,] inputs = new double[vrpnIncomingSignal.Count, vrpnDimensions];
+            int[] output = new int[vrpnIncomingSignal.Count];
+
+            #region convert to LDA format
+            for (int i = 0; i < vrpnIncomingSignal.Count; i++)
+            {
+                output[i] = Convert.ToInt32((vrpnIncomingSignal[i])[0]);
+
+                for (int j = 1; j < vrpnDimensions + 1; j++)
+                {
+                    inputs[i, j - 1] = (vrpnIncomingSignal[i])[j];
+                }
+            }
+            #endregion
+
+            _lda = new LinearDiscriminantAnalysis(inputs, output);
+
+            // Compute the analysis
+            _lda.Compute();
+
+            double[,] projection = _lda.Transform(inputs);
+
+            int vector_count = projection.GetLength(0);
+            int dimensions = projection.GetLength(1);
+
+            // convert for NN format
+            double[][] input2 = new double[vector_count][];
+            double[][] output2 = new double[vector_count][];
+
+            #region convert to NN format
+            for (int i = 0; i < input2.Length; i++)
+            {
+                input2[i] = new double[projection.GetLength(1)];
+                for (int j = 0; j < projection.GetLength(1); j++)
+                {
+                    input2[i][j] = projection[i, j];
+                }
+
+                output2[i] = new double[1];
+
+                //we turn classes from ints to doubles in the range [0..1], because we use sigmoid for the NN
+                output2[i][0] = Convert.ToDouble(output[i]) / 10;
+            }
+            #endregion
+
+            // create neural network
+            _network = new ActivationNetwork(
+                new SigmoidFunction(2),
+                dimensions, // inputs neurons in the network
+                dimensions, // neurons in the first layer
+                1); // one neuron in the second layer
+
+            // create teacher
+            BackPropagationLearning teacher = new BackPropagationLearning(_network);
+
+            // train
+            int p = 0;
+            while (true)
+            {
+                // run epoch of learning procedure
+                double error = teacher.RunEpoch(input2, output2);
+
+                p++;
+                if (p > 100) break;
+                // check error value to see if we need to stop
+
+
+            }
+
+            //now we have a model of a NN+LDA which we can use for classification
+            //model = new AdastraMachineLearningModel(lda, network);
+            //model.ActionList = actions;
         }
 
         public int Classify(double[] input)
--- trunk/src/Adastra/TrainForm.cs
+++ trunk/src/Adastra/TrainForm.cs
@@ -106,84 +106,8 @@ namespace Adastra
             if (vrpnIncomingSignal.Count == 0) { MessageBox.Show("You need to first record some data for specific action"); return; }
             #endregion
 
-            #region prepare data
-            // randomize vectors positions
-            // split in training and validation sets 
-            // train NN until validation set
-            #endregion
-
-            double[,] inputs = new double[vrpnIncomingSignal.Count, vrpnDimensions];
-            int[] output = new int[vrpnIncomingSignal.Count];
-
-            #region convert to LDA format
-            for(int i=0;i<vrpnIncomingSignal.Count;i++)
-            {
-                output[i] = Convert.ToInt32((vrpnIncomingSignal[i])[0]);
-
-                for (int j = 1; j < vrpnDimensions+1; j++)
-                {
-                    inputs[i, j-1] = (vrpnIncomingSignal[i])[j];
-                }
-            }
-            #endregion
-
-            var lda = new LinearDiscriminantAnalysis(inputs, output);
-
-            // Compute the analysis
-            lda.Compute();
-
-            double[,] projection = lda.Transform(inputs);
-             
-            int vector_count = projection.GetLength(0);
-            int dimensions = projection.GetLength(1);
-
-            // convert for NN format
-            double[][] input2 = new double[vector_count][];
-            double[][] output2 = new double[vector_count][];
-            
-            #region convert to NN format
-            for (int i = 0; i < input2.Length; i++)
-            {
-                input2[i] = new double[projection.GetLength(1)];
-                for (int j = 0; j < projection.GetLength(1); j++)
-                {
-                    input2[i][j] = projection[i, j];
-                }
-
-                output2[i] = new double[1];
-
-                //we turn classes from ints to doubles in the range [0..1], because we use sigmoid for the NN
-                output2[i][0] = Convert.ToDouble(output[i]) / 10;
-            }
-            #endregion
-
-            // create neural network
-            ActivationNetwork network = new ActivationNetwork(
-                new SigmoidFunction(2),
-                dimensions, // inputs neurons in the network
-                dimensions, // neurons in the first layer
-                1); // one neuron in the second layer
-
-            // create teacher
-            BackPropagationLearning teacher = new BackPropagationLearning(network);
-            
-            // train
-            int p = 0;
-            while (true)
-            {
-                // run epoch of learning procedure
-                double error = teacher.RunEpoch(input2, output2);
-
-                p++;
-                if (p > 100) break;
-                // check error value to see if we need to stop
-                
-                
-            }
-
-            //now we have a model of a NN+LDA which we can use for classification
-            model = new AdastraMachineLearningModel(lda,network);
-            model.ActionList = actions;
+            AdastraMachineLearningModel m = new AdastraMachineLearningModel();
+            m.Train(vrpnIncomingSignal, vrpnDimensions);
         }
 
         private void buttonSaveModel_Click(object sender, EventArgs e)
