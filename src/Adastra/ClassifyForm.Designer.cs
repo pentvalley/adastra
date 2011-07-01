2e9bb707ed150aa6f8ae5da3508a39b601a70cb7
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-07-01 22:09:51 +0000

    Classification form completed. Time for a beta release.

--- src/Adastra/ClassifyForm.Designer.cs
+++ src/Adastra/ClassifyForm.Designer.cs
@@ -44,9 +44,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(9, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(120, 13);
+            this.label1.Size = new System.Drawing.Size(125, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Trained classifier model:";
+            this.label1.Text = "Trained classifier models:";
             // 
             // buttonStartProcessing
             // 
