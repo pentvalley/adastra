3becddd0ffd9c4aff2b71a66807f7ff7b3315e4e
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-07-25 18:52:50 +0000

    Added non-negative matrix factorization.

---
+++ src/Adastra/Algorithms/Matrix.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NNMF
+{
+    /// <summary>
+    /// A simple matrix type that implements transposition, multiplication and fast division
+    /// </summary>
+    public class Matrix
+    {
+        protected float[,] _data;
+        protected int _x, _y;
+
+        public Matrix(int x, int y)
+        {
+            _x = x;
+            _y = y;
+            _data = new float[x, y];
+        }
+
+        public Matrix(float[,] val)
+            : this(val.GetLength(1), val.GetLength(0))
+        {
+            for (int j = 0; j < _y; j++)
+            {
+                for (int i = 0; i < _x; i++)
+                {
+                    _data[i, j] = val[j, i];
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < _y; j++)
+            {
+                for (int i = 0; i < _x; i++)
+                {
+                    if (sb.Length > 0)
+                        sb.Append(", ");
+                    sb.Append(_data[i, j].ToString());
+                }
+                sb.Append(" : ");
+            }
+            return sb.ToString();
+        }
+
+        public int ShapeX
+        {
+            get
+            {
+                return _x;
+            }
+        }
+
+        public int ShapeY
+        {
+            get
+            {
+                return _y;
+            }
+        }
+
+        public void Set(int x, int y, float val)
+        {
+            _data[x, y] = val;
+        }
+
+        public float Get(int x, int y)
+        {
+            return _data[x, y];
+        }
+
+        public float this[int x, int y]
+        {
+            get
+            {
+                return Get(x, y);
+            }
+            set
+            {
+                Set(x, y, value);
+            }
+        }
+
+        public Matrix Multiply(Matrix m)
+        {
+            if (ShapeX != m.ShapeY)
+                return null;
+
+            Matrix ret = new Matrix(m.ShapeX, ShapeY);
+            for (int i = 0; i < m.ShapeX; i++)
+            {
+                for (int j = 0; j < ShapeY; j++)
+                {
+                    float val = 0;
+                    for (int k = 0; k < ShapeX; k++)
+                        val += (_data[k, j] * m._data[i, k]);
+                    ret._data[i, j] = val;
+                }
+            }
+            return ret;
+        }
+
+        public Matrix FastMultiply(Matrix m)
+        {
+            if (!(ShapeX == m.ShapeX && ShapeY == m.ShapeY))
+                return null;
+
+            Matrix ret = new Matrix(ShapeX, ShapeY);
+            for (int i = 0; i < ShapeX; i++)
+            {
+                for (int j = 0; j < ShapeY; j++)
+                    ret._data[i, j] = (_data[i, j] * m._data[i, j]);
+            }
+            return ret;
+        }
+
+        public Matrix FastDivide(Matrix m)
+        {
+            if (!(ShapeX == m.ShapeX && ShapeY == m.ShapeY))
+                return null;
+
+            Matrix ret = new Matrix(ShapeX, ShapeY);
+            for (int i = 0; i < ShapeX; i++)
+            {
+                for (int j = 0; j < ShapeY; j++)
+                {
+                    float div = m._data[i, j];
+                    if (div != 0)
+                        ret._data[i, j] = (_data[i, j] / div);
+                    else
+                        ret._data[i, j] = 0;
+                }
+            }
+            return ret;
+        }
+
+        public void MultiplyByScalar(float val)
+        {
+            for (int i = 0; i < ShapeX; i++)
+                for (int j = 0; j < ShapeY; j++)
+                    _data[i, j] = (_data[i, j] * val);
+        }
+
+        public Matrix Transpose()
+        {
+            Matrix ret = new Matrix(ShapeY, ShapeX);
+            for (int i = 0; i < ShapeX; i++)
+            {
+                for (int j = 0; j < ShapeY; j++)
+                    ret._data[j, i] = _data[i, j];
+            }
+            return ret;
+        }
+
+        public Matrix GetRowAsMatrix(int y)
+        {
+            Matrix ret = new Matrix(_x, 1);
+            for (int i = 0; i < _x; i++)
+                ret[i, 0] = _data[i, y];
+            return ret;
+        }
+
+        public Matrix GetColumnAsMatrix(int x)
+        {
+            Matrix ret = new Matrix(1, _y);
+            for (int i = 0; i < _y; i++)
+                ret[0, i] = _data[x, i];
+            return ret;
+        }
+
+        public float[] GetRow(int y)
+        {
+            int numCols = ShapeX;
+            float[] ret = new float[numCols];
+            for (int i = 0; i < numCols; i++)
+                ret[i] = Get(i, y);
+            return ret;
+        }
+
+        public float[] GetColumn(int x)
+        {
+            int numRows = ShapeY;
+            float[] ret = new float[numRows];
+            for (int i = 0; i < numRows; i++)
+                ret[i] = Get(x, i);
+            return ret;
+        }
+
+        public class Vector
+        {
+            public float[] data;
+
+            public Vector(float[] data)
+            {
+                this.data = data;
+            }
+            public double CosineSimilarity(Vector otherVector)
+            {
+                double ret = 0;
+                float[] otherData = otherVector.data;
+                int size = Math.Min(data.Length, otherData.Length);
+                for (int i = 0; i < size; i++)
+                    ret += (data[i] * otherData[i]);
+                double magnitude = (Magnitude * otherVector.Magnitude);
+                if (magnitude != 0)
+                    return ret / magnitude;
+                else
+                    return ret;
+            }
+            public double Magnitude
+            {
+                get
+                {
+                    double ret = 0;
+                    foreach (float val in data)
+                        ret += Math.Pow(val, 2);
+                    return Math.Sqrt(ret);
+                }
+            }
+            public void Add(float[] otherData)
+            {
+                int size = Math.Min(data.Length, otherData.Length);
+                for (int i = 0; i < size; i++)
+                    data[i] += otherData[i];
+            }
+        }
+
+        public Matrix ColumnDotProducts
+        {
+            get
+            {
+                int numCols = ShapeX;
+                Matrix ret = new Matrix(numCols, numCols);
+                List<Vector> columnList = new List<Vector>(numCols);
+                for (int i = 0; i < numCols; i++)
+                    columnList.Add(new Vector(GetColumn(i)));
+                for (int i = 0; i < numCols; i++)
+                {
+                    for (int j = 0; j < numCols; j++)
+                    {
+                        if (i == j)
+                            ret[i, j] = 0;
+                        else
+                            ret[i, j] = (float)columnList[i].CosineSimilarity(columnList[j]);
+                    }
+                }
+                return ret;
+            }
+        }
+    }
+}
--- src/Adastra/MainForm.Designer.cs
+++ src/Adastra/MainForm.Designer.cs
@@ -29,31 +29,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("EEG signal");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Claffication output from OpenVibe");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Display", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2});
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Using OpenVibe\'s feature aggegator");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Using Adastra\'s feature aggregator");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Train", new System.Windows.Forms.TreeNode[] {
-            treeNode4,
-            treeNode5});
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Using OpenVibe\'s feature aggegator");
-            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Using Adastra\'s feature aggregator");
-            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Classify", new System.Windows.Forms.TreeNode[] {
-            treeNode7,
-            treeNode8});
-            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Classification", new System.Windows.Forms.TreeNode[] {
-            treeNode6,
-            treeNode9});
-            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Mouse cursor");
-            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Device control", new System.Windows.Forms.TreeNode[] {
-            treeNode11});
-            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Scenario", new System.Windows.Forms.TreeNode[] {
-            treeNode3,
-            treeNode10,
-            treeNode12});
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("EEG signal");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Claffication output from OpenVibe");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Display", new System.Windows.Forms.TreeNode[] {
+            treeNode14,
+            treeNode15});
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Using OpenVibe\'s feature aggegator");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Using Adastra\'s feature aggregator");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Train", new System.Windows.Forms.TreeNode[] {
+            treeNode17,
+            treeNode18});
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Using OpenVibe\'s feature aggegator");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Using Adastra\'s feature aggregator");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Classify", new System.Windows.Forms.TreeNode[] {
+            treeNode20,
+            treeNode21});
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Classification", new System.Windows.Forms.TreeNode[] {
+            treeNode19,
+            treeNode22});
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Mouse cursor");
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("Device control", new System.Windows.Forms.TreeNode[] {
+            treeNode24});
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("Scenario", new System.Windows.Forms.TreeNode[] {
+            treeNode16,
+            treeNode23,
+            treeNode25});
             this.buttonStart = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
@@ -223,12 +223,14 @@
             this.comboBoxScenarioType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxScenarioType.FormattingEnabled = true;
             this.comboBoxScenarioType.Items.AddRange(new object[] {
-            "1. Display: multi-channel EEG from OpenVibe",
+            "1. Display: chart multi-channel EEG signal streamed by OpenVibe",
             "2. Display: LDA/SVM classification output from OpenVibe",
-            "3. Train:  using OpenVibe\'s feature aggegator + Adastra\'s LDA/MLP trainer (relate" +
-                "d scenario 4)",
+            "3. Train:  using OpenVibe\'s feature aggegator + Adastra\'s LDA/MLP/SVM trainer (re" +
+                "lated scenario 4)",
             "4. Display: EEG classification using OpenVibe\'s feature aggegator + Adastra\'s LDA" +
-                "/MLP classifier (related scenario 3)"});
+                "/MLP/SVM classifier (related scenario 3)",
+            "(5). Train: train for mental condition or neurodegenerative disease. ",
+            "(6). Display: result from 5"});
             this.comboBoxScenarioType.Location = new System.Drawing.Point(90, 41);
             this.comboBoxScenarioType.Name = "comboBoxScenarioType";
             this.comboBoxScenarioType.Size = new System.Drawing.Size(615, 21);
@@ -239,34 +241,34 @@
             // 
             this.treeView1.Location = new System.Drawing.Point(12, 212);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Node2";
-            treeNode1.Text = "EEG signal";
-            treeNode2.Name = "Node6";
-            treeNode2.Text = "Claffication output from OpenVibe";
-            treeNode3.Name = "Node1";
-            treeNode3.Text = "Display";
-            treeNode4.Name = "Node7";
-            treeNode4.Text = "Using OpenVibe\'s feature aggegator";
-            treeNode5.Name = "Node8";
-            treeNode5.Text = "Using Adastra\'s feature aggregator";
-            treeNode6.Name = "Node4";
-            treeNode6.Text = "Train";
-            treeNode7.Name = "Node11";
-            treeNode7.Text = "Using OpenVibe\'s feature aggegator";
-            treeNode8.Name = "Node12";
-            treeNode8.Text = "Using Adastra\'s feature aggregator";
-            treeNode9.Name = "Node5";
-            treeNode9.Text = "Classify";
-            treeNode10.Name = "Node3";
-            treeNode10.Text = "Classification";
-            treeNode11.Name = "Node10";
-            treeNode11.Text = "Mouse cursor";
-            treeNode12.Name = "Node9";
-            treeNode12.Text = "Device control";
-            treeNode13.Name = "Node0";
-            treeNode13.Text = "Scenario";
+            treeNode14.Name = "Node2";
+            treeNode14.Text = "EEG signal";
+            treeNode15.Name = "Node6";
+            treeNode15.Text = "Claffication output from OpenVibe";
+            treeNode16.Name = "Node1";
+            treeNode16.Text = "Display";
+            treeNode17.Name = "Node7";
+            treeNode17.Text = "Using OpenVibe\'s feature aggegator";
+            treeNode18.Name = "Node8";
+            treeNode18.Text = "Using Adastra\'s feature aggregator";
+            treeNode19.Name = "Node4";
+            treeNode19.Text = "Train";
+            treeNode20.Name = "Node11";
+            treeNode20.Text = "Using OpenVibe\'s feature aggegator";
+            treeNode21.Name = "Node12";
+            treeNode21.Text = "Using Adastra\'s feature aggregator";
+            treeNode22.Name = "Node5";
+            treeNode22.Text = "Classify";
+            treeNode23.Name = "Node3";
+            treeNode23.Text = "Classification";
+            treeNode24.Name = "Node10";
+            treeNode24.Text = "Mouse cursor";
+            treeNode25.Name = "Node9";
+            treeNode25.Text = "Device control";
+            treeNode26.Name = "Node0";
+            treeNode26.Text = "Scenario";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode13});
+            treeNode26});
             this.treeView1.Size = new System.Drawing.Size(295, 242);
             this.treeView1.TabIndex = 14;
             this.treeView1.Visible = false;
