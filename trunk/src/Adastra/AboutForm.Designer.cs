101bb4f5dc271d40893d8978a6622c5e01ddaf82
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-07-25 18:52:50 +0000

    Added non-negative matrix factorization.

--- trunk/src/Adastra/AboutForm.Designer.cs
+++ trunk/src/Adastra/AboutForm.Designer.cs
@@ -37,9 +37,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(92, 37);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 26);
+            this.label1.Size = new System.Drawing.Size(134, 26);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Adastra 2.0.2";
+            this.label1.Text = "Adastra 2.1";
             // 
             // AboutForm
             // 
