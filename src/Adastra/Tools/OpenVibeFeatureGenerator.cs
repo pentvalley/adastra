837e4bd2e6d3eb20bae63b5f4518fbbd8e76fead
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-08-08 16:58:19 +0000

    Added support for multiple feature generators.

---
+++ src/Adastra/Tools/OpenVibeFeatureGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Vrpn;
+
+namespace Adastra
+{
+    public class OpenVibeFeatureGenerator : IFeatureGenerator
+    {
+        AnalogRemote analog;
+
+        public OpenVibeFeatureGenerator()
+        {
+            analog = new AnalogRemote("openvibe-vrpn@localhost");
+            analog.AnalogChanged += new AnalogChangeEventHandler(analog_AnalogChanged);
+            analog.MuteWarnings = true;
+        }
+
+        /// <summary>
+        /// Request a new value (a set of values)
+        /// </summary>
+        public void Update()
+        {
+            analog.Update();
+        }
+
+        public event ChangedEventHandler Values;
+
+        private void analog_AnalogChanged(object sender, AnalogChangeEventArgs e)
+        {
+            Values(e.Channels);
+        }
+    }
+}
