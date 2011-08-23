89e4f0db0a5e324877fa3978085733a963424f88
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-08-23 14:26:02 +0000

    Adjusting WPF to Adastra.

--- src/Adastra/WPF/OutputWindow.xaml.cs
+++ src/Adastra/WPF/OutputWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows;
 using Microsoft.Research.DynamicDataDisplay;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
 
+using Adastra;
+
 namespace WPF
 {
     
@@ -22,44 +24,54 @@ namespace WPF
         ObservableDataSource<Point> source2 = null;
         ObservableDataSource<Point> source3 = null;
 
-        public OutputWindow()
+        IRawDataReader dataReader;
+
+        public OutputWindow(IRawDataReader p_dataReader)
         {
             InitializeComponent();
+
+            dataReader = p_dataReader;
+
+            dataReader.Values += new RawDataChangedEventHandler(dataReader_Values);
         }
 
-        private void Simulation()
+        long x = 0;
+
+        void dataReader_Values(double[] values)
         {
-            CultureInfo culture = CultureInfo.InvariantCulture;
-            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            Interlocked.Increment(ref x);
+          
+            //CultureInfo culture = CultureInfo.InvariantCulture;
+            //Assembly executingAssembly = Assembly.GetExecutingAssembly();
             // load spim-generated data from embedded resource file
-            const string spimDataName = "Adastra.Repressilator.txt";
-            using (Stream spimStream = executingAssembly.GetManifestResourceStream(spimDataName))
-            {
-                using (StreamReader r = new StreamReader(spimStream))
-                {
-                    string line = r.ReadLine();
-                    while (!r.EndOfStream)
-                    {
-                        line = r.ReadLine();
-                        string[] values = line.Split(',');
+            //const string spimDataName = "Adastra.Repressilator.txt";
+            //using (Stream spimStream = executingAssembly.GetManifestResourceStream(spimDataName))
+            //{
+            //    using (StreamReader r = new StreamReader(spimStream))
+            //    {
+            //        string line = r.ReadLine();
+            //        while (!r.EndOfStream)
+            //        {
+                        //line = r.ReadLine();
+                        //string[] svalues = line.Split(',');
 
-                        double x = Double.Parse(values[0], culture);
-                        double y1 = Double.Parse(values[1], culture);
-                        double y2 = Double.Parse(values[2], culture);
-                        double y3 = Double.Parse(values[3], culture);
+            //double x = Double.Parse(svalues[0], culture);
+            double y1 = values[0];
+            double y2 = values[1];
+            double y3 = values[2];
 
-                        Point p1 = new Point(x, y1);
-                        Point p2 = new Point(x, y2);
-                        Point p3 = new Point(x, y3);
+            Point p1 = new Point(x, y1);
+            Point p2 = new Point(x, y2);
+            Point p3 = new Point(x, y3);
 
-                        source1.AppendAsync(Dispatcher, p1);
-                        source2.AppendAsync(Dispatcher, p2);
-                        source3.AppendAsync(Dispatcher, p3);
+            source1.AppendAsync(Dispatcher, p1);
+            source2.AppendAsync(Dispatcher, p2);
+            source3.AppendAsync(Dispatcher, p3);
 
-                        Thread.Sleep(10); // Long-long time for computations...
-                    }
-                }
-            }
+            Thread.Sleep(10); // Long-long time for computations...
+            //        }
+            //    }
+            //}
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -85,9 +97,14 @@ namespace WPF
             plotter.AddLineGraph(source3, 2, "Data row 3");
 
             // Start computation process in second thread
-            Thread simThread = new Thread(new ThreadStart(Simulation));
+            Thread simThread = new Thread(new ThreadStart(ReadData));
             simThread.IsBackground = true;
             simThread.Start();
+        }
+
+        void ReadData()
+        {
+            dataReader.Update();
         }
     }
 }
