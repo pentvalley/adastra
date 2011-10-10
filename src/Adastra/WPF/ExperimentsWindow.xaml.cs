17e4a39dcb2f822a8903422ddf93ef089044c694
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-10-10 07:54:35 +0000

    testing tasks

--- src/Adastra/WPF/ExperimentsWindow.xaml.cs
+++ src/Adastra/WPF/ExperimentsWindow.xaml.cs
@@ -19,14 +19,14 @@ namespace WPF
 {  
     public partial class ExperimentsWindow : Window
     {
+        private CancellationTokenSource cancellationTokenSource;
+
         Experiment[] workflows = new Experiment[2];
         EEGRecord currentRecord;
 
         public ExperimentsWindow()
         {
             InitializeComponent();
-
-           
         }      
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -46,19 +46,64 @@ namespace WPF
             workflows[0] = new Experiment(currentRecord, new LdaMLP());
             workflows[1] = new Experiment(currentRecord, new LdaSVM());
 
-            foreach (var w in workflows)
+            //foreach (var w in workflows)
+            //{
+            //    Task.Factory.StartNew(() => w.Start());
+            //    w.Completed += new ExperimentCompletedEventHandler(w_Completed);
+            //    w.Progress += new ChangedValuesEventHandler(w_Progress);
+            //}
+
+            //here a list of workflows is converted to a list of tasks with progressbars
+            //one test task is provided below 
+
+            this.cancellationTokenSource = new CancellationTokenSource();
+            var cancellationToken = this.cancellationTokenSource.Token;
+            var progressReporter = new ProgressReporter();
+            var task = Task.Factory.StartNew(() =>
             {
-                Task.Factory.StartNew(() => w.Start());
-                w.Completed += new ExperimentCompletedEventHandler(w_Completed);
-                w.Progress += new ChangedValuesEventHandler(w_Progress);
-            }
+                for (int i = 0; i != 100; ++i)
+                {
+                    // Check for cancellation
+                    cancellationToken.ThrowIfCancellationRequested();
 
-            //Task<AMLearning>[] taskArray = { Task.Factory.StartNew(() => workflows[0].Start()), Task.Factory.StartNew(() => workflows[1].Start()) };
+                    Thread.Sleep(30); // Do some work.
 
-            //AMLearning[] models = new AMLearning[workflows.Length];
+                    // Report progress of the work.
+                    progressReporter.ReportProgress(() =>
+                    {
+                        // Note: code passed to "ReportProgress" can access UI elements freely.
+                        this.bar1.Value = i;
+                    });
+                }
 
-            //for (int i = 0; i < taskArray.Length; i++)
-            //    models[i] = taskArray[i].Result;
+                // After all that work, cause the error if requested.
+                if (false)
+                {
+                    throw new InvalidOperationException("Oops...");
+                }
+
+                // The answer, at last!
+                return 42;
+            }, cancellationToken);
+
+            // ProgressReporter can be used to report successful completion,
+            //  cancelation, or failure to the UI thread.
+            progressReporter.RegisterContinuation(task, () =>
+            {
+                // Update UI to reflect completion.
+                this.bar1.Value = 100;
+
+                // Display results.
+                if (task.Exception != null)
+                    MessageBox.Show("Background task error: " + task.Exception.ToString());
+                else if (task.IsCanceled)
+                    MessageBox.Show("Background task cancelled");
+                else
+                    MessageBox.Show("Background task result: " + task.Result);
+
+                // Reset UI.
+                this.TaskIsComplete();
+            });
 
         }
 
@@ -82,6 +127,33 @@ namespace WPF
         void mrd_ReocordSelected(EEGRecord record)
         {
             currentRecord = record;
+        }
+
+        private void TaskIsComplete()
+        {
+            // Reset UI.
+            this.bar1.Value = 0;
+            //this.startButton.Enabled = true;
+            //this.errorButton.Enabled = true;
+            //this.cancelButton.Enabled = false;
+        }
+
+        //partial void errorButton_Click(object sender, EventArgs e)
+        //{
+        //    // Start the background task with error.
+        //    this.StartBackgroundTask(true);
+
+        //    // Update UI to reflect background task.
+        //    this.TaskIsRunning();
+        //}
+
+        //still not used
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            // Cancel the background task.
+            this.cancellationTokenSource.Cancel();
+
+            // The UI will be updated by the cancellation handler.
         }
     }
 }
