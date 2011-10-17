447029d14712e2835a63fd0ccdf53dc9c7ed9aa2
toncho11 <toncho11@bdb0448b-c86b-b515-12e7-bb2ab79dd5c1>
2011-10-17 07:26:54 +0000

    encog

---
+++ src/Adastra/Algorithms/LdaRBF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adastra.Algorithms
+{
+    public class LdaRBF : AMLearning
+    {
+        /// <summary>
+        /// First Linear Discriminant Analysis (LDA) computations and then Radial Basis Function (RBF) (for training) is applied.
+        /// </summary>
+        public override void Train(List<double[]> outputInput, int inputVectorDimensions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Classify(double[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
