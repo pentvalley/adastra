b76b8daa8a59bfa717e9e175201c154671d20c02
toncho11 <Toncho11@gmail.com>
2013-10-29 17:21:29 +0000

    added blink length check in [100;300]; interval for searching max increased to 100 samples

--- src_matlab/evaluate_sync_quality/calculate_stat.m
+++ src_matlab/evaluate_sync_quality/calculate_stat.m
@@ -16,12 +16,13 @@ for k=1:87
     %Array of first 100 blinks to be processed in sample positions
         leftEyeBlink = 0;
         deltas = [];
-        interval = 10;
+        interval = 100; %around which we search for the max
         
         indSig = -1;
         indBlink = -1;
         indDer = -1;
         
+        %check channels order
         for t =1:3
             if strcmp(EEG.chanlocs(1,t).labels , 'FP1');
                 indSig = t; 
@@ -33,31 +34,75 @@ for k=1:87
                 indDer = t; 
             end;
         end;
-
+        
+        blinksSkippedAtBorder = 0;
+        processedEyeBlinks = 0;
+        noDownEdgeFound = 0;
+        blinksSkippedOnBadLength = 0;
+        
         for i = 2:(EEG.pnts-1)
-            if EEG.data(indBlink,i-1) == 0 && EEG.data(indBlink,i) > 0 
+            if EEG.data(indBlink,i-1) == 0 && EEG.data(indBlink,i) > 0 %detect a rising edge
                 
-                leftEyeBlink = leftEyeBlink +1;
-                
-                if (i > 10) %to avoid index error
+                if (i >= interval)
+                    indDownEdge = find(EEG.data(indBlink,[i+1:i+400]) == 0, 1, 'first');
+                    
+                    leftEyeBlink = leftEyeBlink + 1;
+                    
+                    if (~isempty(indDownEdge))
+                        
+                        samplelength = indDownEdge(1); %length of the blink in samples
+                        timeLength = (samplelength * 1000) / EEG.srate;
+                        
+                        if (timeLength >=100 && timeLength <=300) %to avoid index error
 
-                    [derivMax,maxi] = max(EEG.data(indDer, i-interval : i+interval));
+                            [derivMax,maxi] = max(EEG.data(indDer, i-interval : i+interval));
 
-                    t1 = i;
-                    t2 = i - interval + maxi(1);
-                    delta = t1 - t2;
-
-                    deltas = [deltas delta]; % no more than 100 blinks
-                end
+                            t1 = i; % pos begin of blink according to logical channel
+                            t2 = (i - interval) + maxi(1); % pos nax of the derivative
+                            
+                            if (maxi(1) <= - interval || maxi(1) >= interval)
+                                blinksSkippedAtBorder = blinksSkippedAtBorder + 1;
+                                disp('Blink skipped because it was at the border of the interval');
+                                continue;
+                            end;
+                            
+                            delta = t1 - t2;
+                            if (delta < 0) % peak is before
+                                disp('minus delta detected');
+                            end;
+                            deltas = [deltas delta]; % no more than 100 blinks
+                            processedEyeBlinks = processedEyeBlinks + 1;
+                        else
+                            blinksSkippedOnBadLength = blinksSkippedOnBadLength + 1;
+                        end;
+                    else
+                        noDownEdgeFound = noDownEdgeFound + 1;
+                    end;
+                end;
             end;
             
-            if leftEyeBlink == 100 break; end;
+            if leftEyeBlink == 100 
+                break; 
+            end;
         end;
 
-        disp('leftEyeBlink:');
-        leftEyeBlink
+        disp('All left Eye Blink:');
+        disp(leftEyeBlink);
+        
+        disp('blinks Skipped At Border:');
+        disp(blinksSkippedAtBorder);
+        
+        disp('Blinks skipped because of no down edge:');
+        disp(noDownEdgeFound);
+        
+        disp('Blinks Skipped On Bad Length:');
+        disp(blinksSkippedOnBadLength);
+        
+        disp('Blinks processed:');
+        disp(processedEyeBlinks);
+        
         disp('Saving histogram:');
-        [count bin] = hist(deltas,-10:10);
+        [count bin] = hist(deltas,[-interval:interval])
         
         save([source_data 'k' int2str(k) '_hist_delta.mat'],'count','bin');
 
